4fecf182f360c695e526f5acbd11be87e96b1867
Jon Haase <haase1020@gmail.com>
2020-09-10 20:25:30 -0400

    add framework for key tests for login

--- login_spec.js
+++ login_spec.js
@@ -2,4 +2,12 @@ decribe('/login', () => {
   beforeEach(() => {
     cy.visit('/#/login');
   });
+
+  if (('great with Sign in', () => {})) it('links to #/register', () => {});
+
+  it('reguires email', () => {});
+  it('requires password', () => {});
+  it('requires valid username and password', () => {});
+
+  it('navigates to #/ on successful login', () => {});
 });
